db4c37f980018b65a6e4f1b87edecab17bd8a467
sebas <sebastian11leon@gmail.com>
2025-12-08 17:14:13 -0500

    first commit

---
+++ recursos/app.js
@@ -0,0 +1,50 @@
+var slideNumber = 1;
+// -----------que sea dinamico!!!! identificacion de cant diapos -----------
+const totalSlideNumber = 8; //
+
+document.addEventListener("DOMContentLoaded", function () {
+    mostrarSlide(slideNumber);
+    let totalSlides = document.querySelector('#totalSlides');
+    totalSlideNText = document.createTextNode(totalSlideNumber)
+    totalSlides.append(totalSlideNText);
+});
+
+$(document).ready(function () {
+    $('#btnAvanzar').click(function () {
+        if (slideNumber < totalSlideNumber) {
+            slideNumber++;
+            mostrarSlide(slideNumber);
+        }
+    });
+
+    $('#btnRetroceder').click(function () {
+        if (slideNumber > 1) {
+            slideNumber--;
+            mostrarSlide(slideNumber);
+        }
+    });
+
+    $('.toggle-btn').click(function () {
+        let lista = document.querySelector('#lista');
+        if (lista.hasAttribute('class')) {
+            lista.removeAttribute('class');
+        } else {
+            lista.setAttribute('class', 'active');
+        }
+    });
+});
+
+
+function mostrarSlide(slideNumber) {
+    let slideActual = document.querySelector('#slideActual');
+    if (slideActual.hasChildNodes()) {
+        slideActual.removeChild(slideActual.firstChild);
+    }
+
+    let currrentSlideNumber = document.createTextNode(slideNumber);
+    slideActual.append(currrentSlideNumber);
+
+
+    let imagen = document.querySelector('#container img');
+    imagen.setAttribute('src', "recursos/Diapositiva" + slideNumber + ".jpeg");
+}
