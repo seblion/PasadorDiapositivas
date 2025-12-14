7e83c9763df7df8251f33339d4c77b06c7989fd9
GregorySD1707 <159218483+GregorySD1707@users.noreply.github.com>
2025-12-14 16:52:22 -0500

    Botones deshabilitados al estar en la primera o ultima diapositiva

--- recursos/app.js
+++ recursos/app.js
@@ -1,54 +1,49 @@
 var slideNumber = 1;
 const totalSlideNumber = $('#num_slides').val();
 
-document.addEventListener("DOMContentLoaded", function () {
-    mostrarSlide(slideNumber);
-    let totalSlides = document.querySelector('#totalSlides');
-    totalSlideNText = document.createTextNode(totalSlideNumber)
-    totalSlides.append(totalSlideNText);
-});
-
 $(document).ready(function () {
+    // Inicialización de la diapositiva y el total
+    mostrarSlide();
+    $("#totalSlides").text(totalSlideNumber);
+
+    // Botón de avanzar
     $('#btnAvanzar').click(function () {
-        if (slideNumber < totalSlideNumber) {
-            slideNumber++;
-            mostrarSlide(slideNumber);
+        if (slideNumber >= totalSlideNumber) {
+            return;
         }
+        slideNumber++;
+        mostrarSlide();
     });
 
+    // Botón de retroceder
     $('#btnRetroceder').click(function () {
-        if (slideNumber > 1) {
-            slideNumber--;
-            mostrarSlide(slideNumber);
+        if (slideNumber <= 1) {
+            return;
         }
+        slideNumber--;
+        mostrarSlide();
     });
 
-    $('.toggle-btn').click(function () {
-        let lista = document.querySelector('#lista');
-        if (lista.hasAttribute('class')) {
-            lista.removeAttribute('class');
-        } else {
-            lista.setAttribute('class', 'active');
-        }
+    // Manejo del menú lateral al hacer clic en el icono de hamburguesa
+    $('.toggle-btn').on('click', function () {
+        $("#lista").toggleClass("active");
     });
 
+    // Selección de diapositiva desde el menú lateral
     $('#lista ul li').click(function () {
         slideNumber = $(this).index() + 1;
-        mostrarSlide(slideNumber);
+        mostrarSlide();
     });
 });
 
 
-function mostrarSlide(slideNumber) {
-    let slideActual = document.querySelector('#slideActual');
-    if (slideActual.hasChildNodes()) {
-        slideActual.removeChild(slideActual.firstChild);
-    }
+function mostrarSlide() {
+    $("#slideActual").text(slideNumber);
+    $("#container img").attr("src", "/recursos/Diapositiva" + slideNumber + ".jpeg");
+    manejarUsoDeLosBotones(); // Cada vez que se cambia de diapositiva, se actualizan los botones
+}
 
-    let currrentSlideNumber = document.createTextNode(slideNumber);
-    slideActual.append(currrentSlideNumber);
-
-
-    let imagen = document.querySelector('#container img');
-    imagen.setAttribute('src', "recursos/Diapositiva" + slideNumber + ".jpeg");
+function manejarUsoDeLosBotones() {
+    $('#btnAvanzar').prop('disabled', slideNumber == totalSlideNumber);
+    $('#btnRetroceder').prop('disabled', slideNumber == 1);
 }
